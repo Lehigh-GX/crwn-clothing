f4ed04b8acc9b1fb737ced5fff8b76b3e5d9939e
Xiang Gao <gx7181@gmail.com>
2021-03-17 18:50:15 -0700

    add shop and collection-preview page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/Homepage/homePage.jsx";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/Shop/shop.jsx";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
