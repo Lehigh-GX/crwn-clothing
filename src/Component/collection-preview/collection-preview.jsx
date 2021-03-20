1fe24ef43cc83cd7e87b8b4ee8fdd21eda57e80d
Xiang Gao <gx7181@gmail.com>
2021-03-19 17:53:14 -0700

    add shop collection-item collection-preview

--- src/Component/collection-preview/collection-preview.jsx
+++ src/Component/collection-preview/collection-preview.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./collection-preview.scss";
+import CollectionItem from "../collection-items/collection-item";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
-    <div>
+    <div className="preview">
       {items
         .filter((item, index) => index < 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...itemProps }) => (
+          <CollectionItem key={id} {...itemProps}></CollectionItem>
         ))}
     </div>
   </div>
